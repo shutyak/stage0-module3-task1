06c19c65436b36c02d88c1637e3fc76c2a51163b
vlshutyak <vl.shutyak@gmail.com>
2023-04-12 17:10:54 +0300

    Cosmetic change for check scoring

--- src/main/java/lang/print/gaps/task1/FillGapsApp.java
+++ src/main/java/lang/print/gaps/task1/FillGapsApp.java
@@ -3,6 +3,5 @@ package lang.print.gaps.task1;
 public class FillGapsApp {
     public static void main(String[] args) {
         System.out.println("Fixed");
-              //.        ("Fixed");
     }
 }
